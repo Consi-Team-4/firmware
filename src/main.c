ef336913a70281ce298e85b2118a03c838442db1
gideonrab <gideonrab@gmail.com>
2025-03-27 20:03:15 -0400

    Moved over wait for button press from dev

--- src/main.c
+++ src/main.c
@@ -16,19 +16,21 @@
 #include "lidar.h"
 
 
+const uint start_button = 18; // Pin D6
+
 // === Main ===
 int main() {
     // Initialize USB Serial
     stdio_init_all();
 
-    // Delay to give serial time to connect
-    uint32_t start_ms = to_ms_since_boot(get_absolute_time());
-    while (to_ms_since_boot(get_absolute_time()) < start_ms + 1000) {
-        printf("Waiting for serial...\n");
-        sleep_ms(50);
-    }
+    // // Delay to give serial time to connect for debugging setup code
+    // uint32_t start_ms = to_ms_since_boot(get_absolute_time());
+    // while (to_ms_since_boot(get_absolute_time()) < start_ms + 1000) {
+    //     printf("Waiting for serial...\n");
+    //     sleep_ms(50);
+    // }
 
-    printf("Start ==========================================================================\n");
+    // printf("Start ==========================================================================\n");
 
 
     log_init();
@@ -41,6 +43,14 @@ int main() {
  
     //escSetup(); starts cli for setting esc speeds
     //hearbeat_init();
+
+    while(gpio_get(start_button)) {
+        sleep_ms(50);
+        printf("Waiting for start button.\n");
+        sleep_ms(50);
+        printf("Waiting for start button...\n");
+    }
+
 
     //Start FreeRTOS Scheduler
     vTaskStartScheduler();
